31365331f8bb8b23c4362ebfd76045f74ae3b527
Jinseong Lee <jlee@Jinseongs-MacBook-Air.local>
2025-09-09 23:47:18 -0700

    just update

--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
+// app/page.js
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+    <main style={{ padding: "500px", fontFamily: "sans-serif" }}>
       <h1>월급루팡 유성민 🎉</h1>
-      <p>Next.js + Vercel 배포 성공!</p>
+
+      <div style={{ marginTop: 24 }}>
+        <Link
+          href="/map"
+          style={{
+            display: "inline-block",
+            padding: "10px 16px",
+            borderRadius: 8,
+            border: "1px solid #ddd",
+            textDecoration: "none",
+            fontWeight: 600,
+          }}
+        >
+          지도 보러가기 →
+        </Link>
+      </div>
     </main>
   );
 }
-
